45ae0cc1d0e96a01c23acbd4ea24e84984d0d21a
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2026-09-07 06:52:24 -0300

    adds asyncstorage

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,8 +1,18 @@
 import { Button } from '@/components/Button';
-import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
 import { Filter } from '../../components/Filter';
 import { Input } from '../../components/Input';
 import { Item } from '../../components/Item';
+import { itemsStorage, ItemStorage } from '../../storage/itemsStorage';
 import { FilterStatus } from '../../types/FilterStatus';
 import styles from './styles';
 
@@ -29,28 +39,60 @@ const ITEMS = [
   },
 ];
 
-function handlePress() {
-  return console.log('pressing....');
-}
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState('');
+  const [items, setItems] = useState<ItemStorage[]>([]);
+
+  async function handleAdd() {
+    if (!description.trim())
+      return Alert.alert('Adicionar', 'Informe a descrição para adicionar.');
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+    await itemsStorage.add(newItem);
+    await getItems();
+  }
+
+  async function getItems() {
+    try {
+      const response = await itemsStorage.get();
+      setItems(response);
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível filtar os itens');
+    }
+  }
+  useEffect(() => {
+    getItems();
+  }, []);
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require('@/assets/logo.png')} />
       <View style={styles.form}>
-        <Input placeholder="O que você quero comprar hoje?" />
-        <Button title={'Login'} onPress={handlePress} />
+        <Input
+          placeholder="O que você quero comprar hoje?"
+          onChangeText={setDescription}
+        />
+        <Button title={'Adicionar'} onPress={handleAdd} />
       </View>
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map(s => (
-            <Filter key={s} status={s} isActive />
+            <Filter
+              key={s}
+              status={s}
+              isActive={s === filter}
+              onPress={() => setFilter(s)}
+            />
           ))}
           <TouchableOpacity style={styles.clearButton}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={item => item.id}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
